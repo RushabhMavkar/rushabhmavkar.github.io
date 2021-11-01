b3843ae39fc54991b068b2ae2aa04c375962e829
rushabhmavkar <rushabhmavkar972000@gmail.com>
2021-11-01 15:37:44 +0530

    NEW : Added skills component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './portfolio/navbar/navbar.component';
 import { HeaderComponent } from './portfolio/header/header.component';
+import { SkillsComponent } from './portfolio/skills/skills.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HeaderComponent],
+  declarations: [AppComponent, NavbarComponent, HeaderComponent, SkillsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
